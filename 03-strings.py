6eb96099a045cdec31a75d6dc20f237dd656a7c0
arya <eng.aliyan20@gmail.com>
2021-07-21 14:12:06 +0430

    string cheatSheet

---
+++ 03-strings.py
@@ -0,0 +1,13 @@
+
+name = 'arya'
+age = 19
+
+
+# 1.1
+print(f'My name is {name}, and i am {age}')
+
+# 1.2
+print('My name is ' + name + ', and i am ' + str(age))
+
+# 1.3
+print('My name is %s, and i am %d' %(name, age))
