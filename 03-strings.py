52e071d9096e513cf18bacdd00b0efced569f1ea
arya <eng.aliyan20@gmail.com>
2021-07-21 14:17:55 +0430

    string cheatSheet

--- 03-strings.py
+++ 03-strings.py
@@ -3,11 +3,11 @@ name = 'arya'
 age = 19
 
 
-# 1.1
+# 1.1 (f-string)
 print(f'My name is {name}, and i am {age}')
 
-# 1.2
+# 1.2 (cons)
 print('My name is ' + name + ', and i am ' + str(age))
 
-# 1.3
+# 1.3 (using %)
 print('My name is %s, and i am %d' %(name, age))
