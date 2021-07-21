4c08c3617f047546ce0b734a1125757cc6729789
arya <eng.aliyan20@gmail.com>
2021-07-21 12:48:15 +0430

    syntax cheatSheet

---
+++ 01-syntax.py
@@ -0,0 +1,26 @@
+
+# 1.1
+m = 3
+n = 3
+z = 3
+
+# 1.2
+m = n = z = 3
+
+
+
+# 2.1
+m = input('Enter m')
+n = input('Enter n')
+z = input('Enter z')
+
+# 2.2
+m, n, z = input('Enter m'), input('Enter n'), input('Enter z')
+
+
+
+# 3.1
+a = 'Hello'
+
+# 3.2
+a = "Hello"
