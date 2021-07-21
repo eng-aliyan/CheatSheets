26e359762b8c47f1837315ed9219f32155046d24
arya <eng.aliyan20@gmail.com>
2021-07-21 14:12:25 +0430

    if cheatSheet

---
+++ 04-if.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+text = 'gweukfhwukfhi2fbookref'
+KEY = 'book'
+
+
+# 1.1
+b = KEY in text
+
+# 1.2
+b = text.count(KEY) > 0
+
+# 1.3
+b = not text.find(KEY) == -1
+
+
+# 2.1
+d = (len(KEY) >= 6 and len(KEY) <= 16)
+
+# 2.2
+d = (6 <= len(KEY) <= 16)
